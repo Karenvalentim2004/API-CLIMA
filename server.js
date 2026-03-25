e882ae0bda6475b02ad9f362e6a7050aeb7274e2
Karen Valentim <karenvalentim54@gmail.com>
2026-03-24 21:27:32 -0300

    Ajustes de frontend e mais informações no backend

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
+require('dotenv').config()
+
 const express = require('express')
 const axios = require('axios')
 const cors = require('cors')
 
 const app = express()
+
+const traduzirLua = {
+    "New Moon": "Lua nova",
+    "Waxing Crescent": "Crescente",
+    "First Quarter": "Quarto Crescente",
+    "Waxing Gibbous": "Entre Crescente e Cheia",
+    "Full Moon": "Lua Cheia",
+    "Waning Gibbous": "Lua começando a minguar",
+    "Last Quarter": "Quarto Minguante",
+    "Waning Crescent": "Lua minguante"
+}
+
 app.use(cors())
 
-const API_KEY = ''
+const API_KEY = process.env.API_KEY
 
 app.get('/clima', async (req, res) => {
     const cidade = req.query.cidade
@@ -16,19 +30,25 @@ app.get('/clima', async (req, res) => {
 
     try {
         const response = await axios.get(
-            `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${encodeURIComponent(cidade)}&days=1&lang=pt`
+            `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${encodeURIComponent(cidade)}&days=7&lang=pt`
         )
 
-        const astro = response.data.forecast.forecastday[0].astro
+        const forecast = response.data.forecast.forecastday.map(dia => ({
+            data: dia.date,
+            max: dia.day.maxtemp_c,
+            min: dia.day.mintemp_c,
+            descricao: dia.day.condition.text,
+            nascerSol: dia.astro.sunrise,
+            porSol: dia.astro.sunset,
+            faseLua: traduzirLua[dia.astro.moon_phase]
+        }))
 
         const dados = {
             cidade: response.data.location.name,
             dataHora: response.data.location.localtime,
-            temperatura: response.data.current.temp_c,
-            descricao: response.data.current.condition.text,
-            nascerSol: astro.sunrise,
-            porSol: astro.sunset,
-            faseLua: astro.moon_phase,
+            temperaturaAtual: response.data.current.temp_c,
+            descricaoAtual: response.data.current.condition.text,
+            previsao: forecast
         }
 
         res.json(dados)
